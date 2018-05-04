2416b2f28b0b647a0e2b0b165725a443e2366e55
lvxw <lvxw@fun.tv>
2018-05-04 16:35:53 +0800

    no message

--- src/com/test/wordCount.py
+++ src/com/test/wordCount.py
@@ -1,19 +1,30 @@
-from pyspark import SparkContext, SparkConf
+# -*- coding: UTF-8 -*-
+
+import sys
 
 
-def run():
-    conf = SparkConf().setAppName("helloWorld").setMaster("local[*]")
+def run(input_path, output_path):
+    from pyspark import SparkContext, SparkConf
+    conf = SparkConf()\
+        .set("spark.hadoop.validateOutputSpecs", "false") \
+        .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")\
+        .setAppName("helloWorld")
+
     sc = SparkContext(conf=conf)
-    rdd = sc.textFile('G:/项目/sync_remote_git_repository/learn_pyspark/tmp/logs/words')
+    rdd = sc.textFile(input_path)
 
-    re = rdd.flatMap(lambda x: x.split(' '))\
-        .map(lambda x: (x, 1))\
-        .reduceByKey(lambda x, y: x+y)\
-        .sortBy(lambda x: x[1], False)
+    re = rdd.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(lambda x, y: x+y).sortBy(lambda x: x[1], False)
 
-    re.foreach(lambda x: print(x))
+    re.saveAsTextFile(output_path)
+
+    sc.stop()
 
 
+# 脚本传递的参数
+# D:\python-package E:/project_sync_repository/learn_pyspark/tmp/logs/words E:/project_sync_repository/learn_pyspark/tmp/out/result
 if __name__ == "__main__":
-    print(__name__)
-    # run()
+    python_package = sys.argv[1]
+    input_path = sys.argv[2]
+    output_path = sys.argv[3]
+    sys.path.append(python_package)
+    run(input_path, output_path)
